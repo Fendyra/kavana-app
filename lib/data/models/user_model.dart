e724c6311ac510148b318a028d3088aefcc48f18
fendyra <fendyrarestu2004@gmail.com>
2025-10-28 18:17:39 +0700

    Create Data Model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,37 @@
+
+
+class UserModel {
+  final int id;
+  final String name;
+  final String email;
+  final String password;
+  final DateTime createdAt;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      name: json['name'] ,
+      email: json['email'] ,
+      password: json['password'] ,
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
+}
